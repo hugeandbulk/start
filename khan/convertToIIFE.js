65eeffbcb79615b07663f5d22a1a2610e271c5eb
Huge & Bulk <138213068+hugeandbulk@users.noreply.github.com>
2023-07-31 12:23:52 +0530

    Update convertToIIFE.js

--- khan/convertToIIFE.js
+++ khan/convertToIIFE.js
@@ -1,2 +1,4 @@
 // convertToIIFE.js
 // https://github.com/gfranko/amdclean/blob/master/src/modules/convertToIIFE.js
+// What is a BLOB?
+// BLOB stands for “Binary Large Object” and represents a database type to store binary data. Specifically, examples of BLOBs (Binary Large Objects) are complex files such as images, video, and audio.
