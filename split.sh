af79699867019b097d8cd246cfe601de4a17c452
Huge & Bulk <138213068+hugeandbulk@users.noreply.github.com>
2023-08-18 15:04:34 +0530

    Create split.sh
    https://github.com/d/repo-split/tree/master

---
+++ split.sh
@@ -0,0 +1,51 @@
+#!/bin/bash
+
+set -uxe
+
+if [ $# -ne 2 ]; then
+	echo "expected two arguments given $#"
+	exit 1
+fi
+
+VCAP_REPO=$1
+CLONE_BASE=$2
+CLONE_PATH=$(mktemp -d "$CLONE_BASE/vcap.XXX")
+SCRIPT_DIR=$(cd $(dirname $0) && pwd)
+
+git clone $VCAP_REPO $CLONE_PATH
+cd $CLONE_PATH
+for c in router stager; do
+	git checkout --quiet origin/master
+	git checkout -b small_$c
+	git filter-branch -f --prune-empty --subdirectory-filter $c --index-filter "git rm --quiet --cached --ignore-unmatch -r -f vendor/cache"
+	git push -v git@github.com:d/vcap-$c.git HEAD:master
+done
+
+# trim down staging
+git checkout --quiet origin/master
+git checkout -b small_staging
+git filter-branch -f --prune-empty --subdirectory-filter staging --index-filter "git rm --quiet --cached --ignore-unmatch -r -f vcap_staging\*.gem vendor/cache"
+git push -v git@github.com:d/vcap-staging.git HEAD:master
+
+# package_cache and friends
+git checkout --quiet origin/master
+git checkout -b package_cache-and-friends
+INDEX_FILTER="git rm -r -f --cached --quiet --ignore-unmatch \$(git ls-tree --name-only \$GIT_COMMIT | egrep -v '^package_cache|package_cache_client|LICENSE|NOTICE|.gitignore' ) \*/vendor/cache/\*"
+git filter-branch -f --prune-empty \
+	--index-filter "$INDEX_FILTER" \
+	--parent-filter $SCRIPT_DIR/parent-filter.rb
+git push -v git@github.com:d/package_cache-and-friends.git HEAD:master
+
+git checkout --quiet origin/master
+git checkout -b cchm
+INDEX_FILTER="git rm -r -f --cached --quiet --ignore-unmatch \$(git ls-tree --name-only \$GIT_COMMIT | egrep -v '^cloud_controller|health_manager|LICENSE|NOTICE|.gitignore' ) \*/vendor/cache/\*"
+git filter-branch -f --prune-empty \
+	--index-filter "$INDEX_FILTER" \
+	--parent-filter $SCRIPT_DIR/parent-filter.rb
+git push -v git@github.com:d/cchm.git HEAD:master
+
+git branch -v
+
+
+
+https://github.com/d/repo-split/tree/master
